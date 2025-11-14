096c30999bd913fadbe53380675b4c1a73ea0e54
arslee07 <mail@arslee.me>
2025-11-14 03:52:32 +0300

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# Hare for VSCode
+
+This extension adds basic support for the [Hare](https://harelang.org/) programming language.
+
+## Features
+
+- Syntax highlighting:
+    ![syntax](https://codeberg.org/arslee07/hare-vscode/raw/branch/main/assets/syntax.png)
+- Snippets:
+    ![snippets](https://codeberg.org/arslee07/hare-vscode/raw/branch/main/assets/snippets.gif)
+- Symbols outline for current file:
+    ![outline](https://codeberg.org/arslee07/hare-vscode/raw/branch/main/assets/outline.png)
+- Documentation viewer:
+    ![doc](https://codeberg.org/arslee07/hare-vscode/raw/branch/main/assets/doc.gif)
+- Tasks for building, testing and running the workspace project
+    ![tasks](https://codeberg.org/arslee07/hare-vscode/raw/branch/main/assets/tasks.png)
+
+## Building
+
+```shell
+npm i --dev
+npm run package
+```
+
+## Acknowledges
+
+Thanks to Dubhead for [Universal Ctags optlib for Hare](https://chiselapp.com/user/Dubhead/repository/Ctags_optlib_Hare)!
---
+++ src/extension.ts
@@ -0,0 +1,112 @@
+import * as vscode from 'vscode';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execAsync = promisify(exec);
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.languages.registerDocumentSymbolProvider('hare', new HareDocumentSymbolProvider()),
+        vscode.commands.registerCommand('hare.showDocumentation', showDocumentation),
+        vscode.commands.registerCommand('hare.build', createHareTask('build')),
+        vscode.commands.registerCommand('hare.test', createHareTask('test')),
+        vscode.commands.registerCommand('hare.run', createHareTask('run')),
+    );
+}
+
+/** 
+ * `hare.showDocumentation` command callback.
+ */
+async function showDocumentation() {
+    const identifier = await vscode.window.showInputBox({
+        prompt: 'Enter indentifier name (e.g., bufio, fmt::println, ./sort.ha)',
+        placeHolder: 'fmt::println'
+    });
+
+    if (!identifier) {
+        return;
+    }
+
+    const { haredocExecutable } = vscode.workspace.getConfiguration('hare')
+
+    try {
+        const { stdout } = await execAsync(`${haredocExecutable} -F html -t ${identifier}`);
+        const panel = vscode.window.createWebviewPanel(
+            'haredoc',
+            'Haredoc',
+            vscode.ViewColumn.Beside,
+        )
+        panel.webview.html = stdout;
+    } catch (error: any) {
+        vscode.window.showErrorMessage(`Failed to generate documentation: ${error.message}`)
+    }
+}
+
+/** 
+ * Wrapper for basic task callbacks, e.g. `hare.build`, `hare.test`, etc.
+ */
+function createHareTask(command: string): () => void {
+    const { hareExecutable } = vscode.workspace.getConfiguration('hare')
+
+    const folder = vscode.workspace.workspaceFolders?.[0];
+    if (!folder) {
+        return () => {
+            vscode.window.showErrorMessage("No workspace folder open.");
+        };
+    }
+
+    return () => {
+        const task = new vscode.Task(
+            { type: 'shell' },
+            folder,
+            `Hare ${command}`,
+            'shell',
+            new vscode.ShellExecution(`${hareExecutable} ${command}`)
+        );
+        vscode.tasks.executeTask(task);
+    }
+}
+
+/**
+ * Basic, regex-based symbol provider for Hare source files.
+ */
+class HareDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+    static patterns: { regex: RegExp; kind: vscode.SymbolKind }[] = [
+        { regex: /^(export\s+)?fn\s+([a-zA-Z_][a-zA-Z0-9_]*)/, kind: vscode.SymbolKind.Function },
+        { regex: /^@(test|init|fini)\s+fn\s+([a-zA-Z_][a-zA-Z0-9_]+)/, kind: vscode.SymbolKind.Function },
+        { regex: /^(export\s+)?type\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*struct\s*\{/, kind: vscode.SymbolKind.Struct },
+        { regex: /^(export\s+)?type\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*union\s*\{/, kind: vscode.SymbolKind.Struct },
+        { regex: /^(export\s+)?type\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*enum\s+/, kind: vscode.SymbolKind.Enum },
+        { regex: /^(export\s+)?type\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*!/, kind: vscode.SymbolKind.TypeParameter },
+        { regex: /^(export\s+)?type\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*=/, kind: vscode.SymbolKind.TypeParameter },
+        { regex: /^(export\s+)?def\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*[:=]/, kind: vscode.SymbolKind.Constant },
+        { regex: /^(export\s+)?const\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*[:=]/, kind: vscode.SymbolKind.Constant },
+        { regex: /^(export\s+)?let\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*[:=]/, kind: vscode.SymbolKind.Variable },
+    ];
+
+    provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.SymbolInformation[] | vscode.DocumentSymbol[]> {
+        let symbols: vscode.DocumentSymbol[] = [];
+
+        for (let i = 0; i < document.lineCount; i++) {
+            const line = document.lineAt(i);
+            const text = line.text;
+
+            for (const pattern of HareDocumentSymbolProvider.patterns) {
+                const match = pattern.regex.exec(text);
+                if (match && match[2]) {
+                    const name = match[2];
+                    const range = line.range;
+                    const selectionRange = new vscode.Range(
+                        new vscode.Position(i, match.index + (match[1]?.length || 0)),
+                        new vscode.Position(i, match.index + match[0].length)
+                    );
+                    const symbol = new vscode.DocumentSymbol(name, '', pattern.kind, range, selectionRange)
+                    symbols.push(symbol);
+                    break;
+                }
+            }
+        }
+
+        return symbols;
+    }
+}
